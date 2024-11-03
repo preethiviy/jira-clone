3d53b5ffde29040f72be2e83a97e93ae7fa0ce26
preethiviy <preethivirajyambem@gmail.com>
2024-11-04 01:16:41 +0530

    session middleware

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,24 @@
+'use client';
+
+import { useCurrent } from '@/features/auth/api/use-current';
+import { useLogout } from '@/features/auth/api/use-logout';
+import { useRouter } from 'next/navigation';
+import { useEffect } from 'react';
+
 export default function Home() {
-	return (
-		<div>
-		
-		</div>
-	);
+    const router = useRouter();
+    const { data, isLoading } = useCurrent();
+    const { mutate } = useLogout();
+
+    useEffect(() => {
+        if (!data && !isLoading) {
+            router.push('/sign-in');
+        }
+    }, [data]);
+    return (
+        <div>
+            Only visible to login users
+            <button onClick={() => mutate()}>Logout</button>
+        </div>
+    );
 }
